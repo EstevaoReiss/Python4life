08ba3e750096dcbb090e1844f670754906e7eec2
EstevaoReiss <129298689+EstevaoReiss@users.noreply.github.com>
2025-09-02 21:21:01 -0300

    feat:exer 10

--- aulas/exer10.py
+++ aulas/exer10.py
@@ -1,4 +1,4 @@
 for x in range(11):
     for y in range (11):
-        print(f"({x} x {y} = {x*y})")
-        print("----------------------")
+      print(f"({x} x {y} = {x*y})")
+    print("----------------------")
